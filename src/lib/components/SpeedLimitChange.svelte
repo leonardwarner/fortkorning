1ef994e995a6b0928220e93a210c3ca01dc9ac08
Fabian Mossberg <fabian@webbdesigner.se>
2022-12-20 20:12:36 +0100

    Adding speed limit sign

---
+++ src/lib/components/SpeedLimitChange.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let speedLimit = 120;
+    $: smallFont = speedLimit.toString().length > 2;
+</script>
+
+<div
+    class="bg-yellow-400 select-none w-48 aspect-square rounded-full border-[20px] border-red-600 grid place-content-center font-bold text-8xl"
+    class:text-7xl={smallFont}
+    >
+    {speedLimit}
+</div>
